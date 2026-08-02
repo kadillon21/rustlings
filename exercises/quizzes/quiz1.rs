6e7cd8e6610c27daf2129afafd87d4d446ae7347
kadillon21 <kadillon21@gmail.com>
2026-08-02 15:09:42 -0500

    Completed first quiz

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -12,6 +12,14 @@
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
 
+fn calculate_price_of_apples(x: u32) -> u32 {
+    if x <= 40 {
+        x * 2
+    } else {
+        x 
+    }
+}
+
 fn main() {
     // You can optionally experiment here.
 }
